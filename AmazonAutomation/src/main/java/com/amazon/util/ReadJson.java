3ba85c5602cf22b280aaa52ef1b9cd332488dc31
MW034 <MW034@MW034>
2022-10-29 16:23:10 +0530

    ReadJson file added

---
+++ AmazonAutomation/src/main/java/com/amazon/util/ReadJson.java
@@ -0,0 +1,23 @@
+package com.amazon.util;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class ReadJson {
+
+	public void getJsonData() throws Exception {
+
+		FileInputStream inputStream = new FileInputStream(
+				System.getProperty("user.dir") + "/src/main/java" + "/objectRepository/Config.json");
+		JSONParser parser = new JSONParser();
+		JSONObject jsonobj = (JSONObject) parser.parse(new InputStreamReader(inputStream));
+		UserData uData = new UserData();
+		uData.setBrowser((String) jsonobj.get("browser"));
+		uData.setEnviroment((String) jsonobj.get("enviroment"));
+		uData.setStageUrl((String) jsonobj.get("STAGE"));
+		uData.setProdUrl((String) jsonobj.get("PROD"));
+	}
+}
